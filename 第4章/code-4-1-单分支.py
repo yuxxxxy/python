ac665b357d9d1256f6f3a8af3ec7e23e8e4d4157
yuxy <56705404+yuxxxxy@users.noreply.github.com>
2025-01-13 23:37:17 +0800

    Add files via upload

---
+++ 第4章/code-4-1-单分支.py
@@ -0,0 +1,16 @@
+weather = "下雨"
+if(weather == "下雨"):
+    print("记得带伞")
+else:
+    print("天气晴，玩的开心")
+weather = "晴天"
+if (weather == "下雨"):
+    print("记得带伞")
+else:
+    print("天气晴，玩的开心")
+
+age = 19
+if(age >= 18):
+    print("开始上网咯")
+else:
+    print("小臂不能上网")
